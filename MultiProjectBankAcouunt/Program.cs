09655b64e4b77847ccaee98b4df4be272801296f
steven ballard <stevenballardstudios@gmail.com>
2019-10-21 13:39:59 -0700

    Add project files.

---
+++ MultiProjectBankAcouunt/Program.cs
@@ -0,0 +1,24 @@
+﻿using BankAccountBuisnessLogic;
+using System;
+using static System.Console;
+
+namespace MultiProjectBankAcouunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Welcome to Joe's Bank!");
+            WriteLine("Please enter an account number: ");
+
+            string accNum = Console.ReadLine();
+            BankAccount acc = new BankAccount(accNum);
+
+            WriteLine("\n\nEnter an amount to deposit:");
+            double amount = Convert.ToDouble(ReadLine());
+            acc.Deposit(amount);
+            WriteLine($"The new balance is: {acc.Balance}");
+            ReadKey();
+        }
+    }
+}
